dc15e0c5b2698e0ff6cbf816a35b32374e635ba9
Judith Alejandra Fogacho Suquilanda <judith.fogacho@udla.edu.ec>
2024-11-28 11:28:20 -0500

    Creacion Proyecto

---
+++ FogachoHeladeriaApp/obj/Debug/net8.0-android/android/src/crc64c7de115eeffa4992/MainActivity.java
@@ -0,0 +1,48 @@
+package crc64c7de115eeffa4992;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("FogachoHeladeriaApp.MainActivity, FogachoHeladeriaApp", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("FogachoHeladeriaApp.MainActivity, FogachoHeladeriaApp", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("FogachoHeladeriaApp.MainActivity, FogachoHeladeriaApp", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+		}
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
